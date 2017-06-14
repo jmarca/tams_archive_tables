5aab5489231e1cbf583cc495a1417115bfa54ee0
James E. Marca <james@activimetrics.com>
2017-06-14 11:37:11 -0700

    added ability to grab just one detector, time range

--- test/test_01_get_tables.js
+++ test/test_01_get_tables.js
@@ -2,7 +2,10 @@
 
 // jeez.  have to create the stuff and all that
 
-const get_tables = require('../lib/get_tables').get_tables
+const get_tables_lib = require('../lib/get_tables')
+const get_tables = get_tables_lib.get_tables
+const get_tables_for_detector = get_tables_lib.get_tables_for_detector
+
 const get_pool = require('psql_pooler').get_pool
 
 const tap = require('tap')
@@ -41,7 +44,7 @@ const station_ids = [10001,
                      7002,
                      7003]
 
-tap.plan(3)
+tap.plan(4)
 
 tap.test('get_tables function exists',function (t) {
     t.plan(1)
@@ -77,13 +80,13 @@ const test_query = async (_config,pool) => {
 
             t.plan(planned_tests)
             const task = await get_tables(config,client)
-            //console.log(task)
+            // console.log('task result',task.signaturearchives)
             t.ok(task.signaturearchives,'there is a signaturearchives object')
             t.ok(task.signaturearchives.size = station_ids.length
                  ,'got expected number of map entries')
 
             const archive_map = task.signaturearchives
-            //console.log(archive_map.size)
+            // console.log(archive_map.size)
             archive_map.forEach( (table_entry,detectorid) => {
                 // console.log(detectorid,table_entry,counter++,planned_tests)
                 // planned_tests -= 2
@@ -116,6 +119,157 @@ const test_query = async (_config,pool) => {
         await client.query("ROLLBACK;")
         await client.release()
     }
+
+
+    // testing get_tables_for_detector
+
+    client = await pool.connect()
+    await client.query("BEGIN;")
+    try {
+        let counter = 0
+        await tap.test('run with client, one detector',async function(t){
+            let config = Object.assign({},_config)
+            let planned_tests = 2 + 9 + 7 + 7 + 3
+            t.plan(planned_tests)
+
+            // first, without detstaid, shoudl throw
+            try {
+                await get_tables_for_detector(config,client)
+                t.fail('should have thrown')
+            }catch (e){
+                t.pass('threw okay')
+                t.ok(/detstaid required/.test(e),'got expected error message')
+            }
+
+            config.detstaid = 6002
+            let task = await get_tables_for_detector(config,client)
+            // console.log('task result',task.signaturearchives)
+            t.ok(task.signaturearchives,'there is a signaturearchives object')
+            t.ok(task.signaturearchives.size = 1
+                 ,'got expected number of map entries')
+
+            let archive_map = task.signaturearchives
+            // console.log(archive_map.size)
+            archive_map.forEach( (table_entry,detectorid) => {
+                // console.log(detectorid,table_entry,counter++,planned_tests)
+                // planned_tests -= 2
+
+                t.ok(station_ids.indexOf(detectorid)>-1,'got a meaningful key')
+                t.ok(table_entry.size>0,'got a non-empty map')
+                t.is(table_entry.size,2,'should have two db tables for detector 6002')
+
+                table_entry.forEach((value,key)=>{
+                   // console.log(detectorid,table_entry,counter++,planned_tests)
+                   //  planned_tests -= 2
+
+                    let value_keys = Object.keys(value )
+
+                    t.same(['mintime','maxtime'],value_keys
+                           ,'map has expected entries')
+                    t.ok(tablenames.indexOf(key)>-1,'got a meaningful key')
+                    return null
+                })
+
+                return null
+            })
+
+            // now set a minimum time
+            config = Object.assign({},_config)
+            config.detstaid = 6002
+            config.mintime = '2015-12-31 20:00:16.001'
+            //config.maxtime = '2015-12-31 20:00:16.001'
+
+            task = await get_tables_for_detector(config,client)
+            // console.log('task result',task.signaturearchives)
+            t.ok(task.signaturearchives,'there is a signaturearchives object')
+            t.ok(task.signaturearchives.size = 1
+                 ,'got expected number of map entries')
+
+            archive_map = task.signaturearchives
+            // console.log(archive_map.size)
+            archive_map.forEach( (table_entry,detectorid) => {
+                // console.log(detectorid,table_entry,counter++,planned_tests)
+                // planned_tests -= 2
+
+                t.ok(station_ids.indexOf(detectorid)>-1,'got a meaningful key')
+                t.ok(table_entry.size>0,'got a non-empty map')
+                t.is(table_entry.size,1,'now should have one db tables for detector 6002')
+
+                table_entry.forEach((value,key)=>{
+                   // console.log(detectorid,table_entry,counter++,planned_tests)
+                   //  planned_tests -= 2
+
+                    let value_keys = Object.keys(value )
+
+                    t.same(['mintime','maxtime'],value_keys
+                           ,'map has expected entries')
+                    t.ok(tablenames.indexOf(key)>-1,'got a meaningful key')
+                    return null
+                })
+
+                return null
+            })
+
+            // now set a maximum time
+            config = Object.assign({},_config)
+            config.detstaid = 6002
+            config.maxtime = '2016-01-01 00:00:00.001'
+
+            task = await get_tables_for_detector(config,client)
+            // console.log('task result',task.signaturearchives)
+            t.ok(task.signaturearchives,'there is a signaturearchives object')
+            t.ok(task.signaturearchives.size = 1
+                 ,'got expected number of map entries')
+
+            archive_map = task.signaturearchives
+            // console.log(archive_map.size)
+            archive_map.forEach( (table_entry,detectorid) => {
+                // console.log(detectorid,table_entry,counter++,planned_tests)
+                // planned_tests -= 2
+
+                t.ok(station_ids.indexOf(detectorid)>-1,'got a meaningful key')
+                t.ok(table_entry.size>0,'got a non-empty map')
+                t.is(table_entry.size,1,'now should have one db tables for detector 6002')
+
+                table_entry.forEach((value,key)=>{
+                   // console.log(detectorid,table_entry,counter++,planned_tests)
+                   //  planned_tests -= 2
+
+                    let value_keys = Object.keys(value )
+
+                    t.same(['mintime','maxtime'],value_keys
+                           ,'map has expected entries')
+                    t.ok(tablenames.indexOf(key)>-1,'got a meaningful key')
+                    return null
+                })
+
+                return null
+            })
+
+            // now set a min and max time that isn't covered
+            config = Object.assign({},_config)
+            config.detstaid = 6002
+            config.mintime = '2016-01-02 00:00:00.001'
+            config.maxtime = '2016-01-02 00:01:00.001'
+
+            task = await get_tables_for_detector(config,client,true)
+            t.ok(task.signaturearchives,'there is a signaturearchives object')
+            t.ok(task.signaturearchives.size = 1
+                 ,'got a non-empty map')
+            t.is( task.signaturearchives.get(6002).size, 0
+                 ,'but nothing in it')
+
+
+            t.end()
+        })
+    }catch (e){
+        throw (e)
+    }finally{
+        await client.query("ROLLBACK;")
+        await client.release()
+    }
+
+
     return null
 
 }
