db216bff8b5b8e6c4be0625818ff6b8cdbe58a5e
James E. Marca <james@activimetrics.com>
2017-06-14 22:13:53 -0700

    add to get_tables_for_detector to index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-exports.get_tables = require('./lib/get_tables.js').get_tables
+const mylib = require('./lib/get_tables.js').get_tables
+exports.get_tables = mylib.get_tables
+exports.get_tables_for_detector = mylib.get_tables_for_detector
